84fb00a060a43ddbe3dc6eab8df76906b1dde315
AmirAli <amirali23mohammadi2@gmail.com>
2026-08-04 14:30:33 +0330

    add alias for running tui

--- tui/src/app/events.rs
+++ tui/src/app/events.rs
@@ -1,23 +1,21 @@
-use std::io;
-
+use color_eyre::eyre::Context;
 use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind};
 use futures_util::StreamExt;
-use thiserror::Error;
 
 use crate::app::App;
 
 impl App {
-    pub async fn handle_event(&mut self) -> Result<(), HandleEventsErr> {
-        if let Some(event) = self.event_stream.next().await {
-            match event? {
-                Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                    self.handle_key_event(key_event);
-                    Ok(())
-                }
-                _ => Ok(()),
+    pub async fn handle_event(&mut self) -> color_eyre::Result<()> {
+        let Some(event) = self.event_stream.next().await else {
+            return Ok(());
+        };
+
+        match event.wrap_err("io error reading event")? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event);
+                Ok(())
             }
-        } else {
-            Ok(())
+            _ => Ok(()),
         }
     }
 
@@ -27,10 +25,4 @@ impl App {
             _ => {}
         }
     }
-}
-
-#[derive(Error, Debug)]
-pub enum HandleEventsErr {
-    #[error("io error reading event: {0}")]
-    Io(#[from] io::Error),
 }

3d8b8cb9a0fc8f5a4a3154bb193f4d50855ad70c
AmirAli <amirali23mohammadi2@gmail.com>
2026-08-05 00:41:36 +0330

    add logging to tui

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,3 +1,6 @@
+use tracing::info;
+use tracing_subscriber::EnvFilter;
+
 use crate::app::App;
 
 mod app;
@@ -5,11 +8,23 @@ mod ui;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
+    let file_appender = tracing_appender::rolling::daily("./logs", "tui.log");
+
+    let (non_blocking, _gaurd) = tracing_appender::non_blocking(file_appender);
+
+    tracing_subscriber::fmt()
+        .with_writer(non_blocking)
+        .with_ansi(false)
+        .with_env_filter(EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info")))
+        .init();
+
     color_eyre::install()?;
 
     let mut terminal = ratatui::init();
 
+    info!("app starting...");
     let app_result = App::new().run(&mut terminal).await;
+    info!("app returning...");
 
     ratatui::restore();
 

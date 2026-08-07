15d97c7a492f5b46f8b59cb37cf33716b39b3d71
AmirAli <amirali23mohammadi2@gmail.com>
2026-08-07 22:42:38 +0330

    remove v1

--- core/src/json_rpc/version.rs
+++ core/src/json_rpc/version.rs
@@ -3,8 +3,6 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize)]
 #[cfg_attr(test, derive(Debug, PartialEq, Eq))]
 pub enum JsonRpcVersion {
-    #[serde(rename = "1.0")]
-    V1,
     #[serde(rename = "2.0")]
     V2,
 }
@@ -15,18 +13,12 @@ mod tests {
 
     #[test]
     fn test_json_rpc_version_serialize() {
-        let v1 = JsonRpcVersion::V1;
-        assert_eq!(serde_json::to_string(&v1).unwrap(), r#""1.0""#);
-
         let v2 = JsonRpcVersion::V2;
         assert_eq!(serde_json::to_string(&v2).unwrap(), r#""2.0""#);
     }
 
     #[test]
     fn test_json_rpc_version_deserialize() {
-        let v1: JsonRpcVersion = serde_json::from_str(r#""1.0""#).unwrap();
-        assert_eq!(v1, JsonRpcVersion::V1);
-
         let v2: JsonRpcVersion = serde_json::from_str(r#""2.0""#).unwrap();
         assert_eq!(v2, JsonRpcVersion::V2);
 
